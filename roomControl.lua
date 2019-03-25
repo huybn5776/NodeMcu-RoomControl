e7a0247e4f3c5f98b6f00027be73b02ec96afa9d
huybn5776 <aseeme555076@gmail.com>
2019-03-25 22:25:41 +0800

    Simplify code to run without LFS support.

--- roomControl.lua
+++ roomControl.lua
@@ -5,11 +5,3 @@ end
 function roomOff()
   httpGet(config.roomOffUrl, 'roomOff')
 end
-
-function getSp3State(callback)
-  httpGet(config.sp3StateUrl, 'get SP3 state', function(data)
-    local on = string.find(data, '"on_off_status":1') and true or false
-    print('on', on)
-    callback(on)
-  end)
-end
